39de83f2722bbe40f249e2ec6c9aa8dd05187ae7
sotoriz <sotorisfx@gmail.com>
2023-08-08 10:59:26 -0700

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,7 +5,7 @@ const request = require('request');
 const FILMID = process.argv[2];
 
 // Request URL
-const URL_BASE = 'https://swapi-api.hbtn.io/api/films';
+const URL_BASE = 'https://swapi-api.alx-tools.com//api/films';
 
 function doRequest (url) {
   return new Promise(function (resolve, reject) {
